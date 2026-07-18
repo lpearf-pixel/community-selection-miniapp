bee7aae6e4c27aa1f65ea612a70e89e0e36f5ad5
lpearf-pixel <lpearf@gmail.com>
2026-07-18 19:51:53 +0800

    feat(L47): register personal and leader center routes

--- apps/api/src/routes/public/index.ts
+++ apps/api/src/routes/public/index.ts
@@ -6,7 +6,9 @@ import { registerPaymentRoutes } from '../payments.js';
 import { registerRefundRoutes } from '../refunds.js';
 import { registerRewardRoutes } from '../rewards.js';
 import { registerPublicAfterSaleRoutes } from '../after-sales.js';
+import { registerMeCenterRoutes } from '../me/center.js';
 import { registerUserOrderRoutes } from '../me/orders.js';
+import { registerLeaderCenterRoutes } from '../leaders/center.js';
 import { registerPublicLocationRoutes } from './locations.js';
 import { registerPublicDeliveryRoutes } from './delivery.js';
 
@@ -21,4 +23,6 @@ export function registerPublicRoutes(app: FastifyInstance) {
   registerRewardRoutes(app);
   registerPublicAfterSaleRoutes(app);
   registerUserOrderRoutes(app);
+  registerMeCenterRoutes(app);
+  registerLeaderCenterRoutes(app);
 }
